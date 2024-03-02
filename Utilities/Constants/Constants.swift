6f03f00db29f76486ba2408b0e0873e82d3dea4b
Ibrahim Nasser <thenasserr@gmail.com>
2024-03-02 18:27:28 +0200

    adding constants struct

---
+++ Utilities/Constants/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  FilmFlix
+//
+//  Created by Ibrahim Nasser Ibrahim on 02/03/2024.
+//
+
+import Foundation
+
+struct Constants {
+    static let imageBase = "https://image.tmdb.org/t/p/w500"
+}
