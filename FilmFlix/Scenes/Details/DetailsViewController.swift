40c554e7618aeb0ad0a5367c5aa991d0e83389b1
Ibrahim Nasser <thenasserr@gmail.com>
2024-03-03 02:53:35 +0200

    finishing details view controller

--- FilmFlix/Scenes/Details/DetailsViewController.swift
+++ FilmFlix/Scenes/Details/DetailsViewController.swift
@@ -8,22 +8,90 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
-
+    
+    var movie: Movie
+    
+    @IBOutlet weak var reviewLabel: UILabel!
+    @IBOutlet weak var middleView: UIView!
+    @IBOutlet weak var mainContainerView: UIStackView!
+    @IBOutlet weak var detailsContainerView: UIStackView!
+    @IBOutlet weak var posterImage: UIImageView!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var popularityLabel: UILabel!
+    @IBOutlet weak var releasedDate: UILabel!
+    @IBOutlet weak var mediaType: UILabel!
+    @IBOutlet weak var movieName: UILabel!
+    @IBOutlet weak var detailsTitle: UILabel!
+    // MARK: - Initialization
+    init(movie: Movie) {
+        self.movie = movie
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureUI()
+        showDishDetails()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func showDishDetails() {
+        let imageURL = (Constants.imageBase + movie.poster_path)
+        posterImage.load(urlString: imageURL)
+        detailsTitle.text = L10n.Details.title
+        movieName.text = movie.original_name ?? movie.original_title
+        mediaType.text = movie.media_type
+        releasedDate.text = movie.release_date
+        popularityLabel.text = String(movie.vote_average)
+        overviewLabel.text = movie.overview
     }
-    */
-
+    
+    private func configureUI() {
+        view.backgroundColor = .black
+        posterImage.posterCustomShape()
+        configureDetailsStackView()
+        configureLabelsUI()
+        configureMiddleView()
+    }
+    
+    private func configureDetailsStackView() {
+        detailsContainerView.layoutMargins = .init(top: 0, left: 10, bottom: 10, right: 10)
+        detailsContainerView.isLayoutMarginsRelativeArrangement = true
+        detailsContainerView.detailsCustomShape()
+        detailsContainerView.backgroundColor = .white
+    }
+    
+    private func configureMiddleView() {
+        middleView.layer.cornerRadius = 15
+        middleView.layer.borderWidth = 2
+        middleView.layer.borderColor = UIColor.black.cgColor
+        middleView.backgroundColor = .white
+        
+        reviewLabel.text = L10n.Details.Review.title
+        reviewLabel.font = .h3
+        reviewLabel.textColor = .black
+    }
+    
+    private func configureLabelsUI() {
+        detailsTitle.font = .h2
+        detailsTitle.textColor = .black
+        
+        movieName.font = .h2
+        movieName.textColor = .black
+        
+        mediaType.font = .regular
+        mediaType.textColor = .gray
+        
+        releasedDate.font = .regular
+        releasedDate.textColor = .gray
+        
+        popularityLabel.font = .regular
+        popularityLabel.textColor = .gray
+        
+        overviewLabel.font = .regular
+        overviewLabel.textColor = .gray
+    }
 }
