2329c7bc655a74eb2b58503a0fb8add765b10773
Ibrahim Nasser <thenasserr@gmail.com>
2024-03-04 23:32:19 +0200

    adding core data context and persistent and the model

--- FilmFlix/AppDelegate.swift
+++ FilmFlix/AppDelegate.swift
@@ -6,6 +6,32 @@
 //
 
 import UIKit
+import CoreData
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate { }
+class AppDelegate: UIResponder, UIApplicationDelegate { 
+    
+    // MARK: - Core Data stack
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "FilmFlixModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
